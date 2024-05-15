7d7066f1e435e1b44818bed271259bfbcb3280fa
“Achraf <achrafassouam@gmail.com>
2024-05-15 14:03:45 +0100

    square.py

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
+""" square module """
+
 
 class square():
-    
+    """ square class """    
     width = 0
     height = 0
 
     
     def __init__(self, *args, **kwargs):
+        """ init for square class """
         for key, value in kwargs.items():
             setattr(self, key, value)
 
@@ -15,13 +18,15 @@ class square():
         return self.width * self.width
 
     def PermiterOfMySquare(self):
+       """ perimiter of the square """
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
+        """ str representation of square """
         return "{}/{}".format(self.width, self.height)
 
 if __name__ == "__main__":
-
+    """ square instance """
     s = square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
